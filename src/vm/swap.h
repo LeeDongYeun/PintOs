98a648d0c690332c19e799c6c0ceb4747e35948d
LeeDongYeun <ledoye@kaist.ac.kr>
2018-11-06 21:51:24 +0900

    swap.c

--- src/vm/swap.h
+++ src/vm/swap.h
@@ -5,6 +5,16 @@
 #include "devices/disk.h"
 #include <bitmap.h>
 
+#define DISK_SECTOR_NUMBER 8
+
+struct bitmap *swap_table;
+struct disk *swap_disk;
+
+struct lock lock_swap;
+
 void swap_init(void);
-void swap_in(void *addr);
-void swap_out(void *addr);
+void swap_out(void *kaddr);
+void swap_in(void *kaddr, size_t swap_table_index);
+void swap_free(void *kaddr);
+
+#endif
